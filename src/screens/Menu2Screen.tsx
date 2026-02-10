457ed47d17b1aa4d26f190afeb52fd06aeb7bde9
jeongbaul <jbu1391@gmail.com>
2026-02-10 19:10:25 +0900

    feat: 메뉴2에 캘린더 기능 추가   - react-native-calendars 라이브러리 통합   - 실제 기기 빌드 스크립트 추가 (npm run device)   - iOS 네이티브 의존성 업데이트

--- src/screens/Menu2Screen.tsx
+++ src/screens/Menu2Screen.tsx
@@ -1,5 +1,7 @@
 import { Text, useColorScheme, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 import { getStyles } from './Menu2Screen.styles';
+
 
 export function Menu2Screen() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -7,8 +9,11 @@ export function Menu2Screen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>메뉴 2</Text>
-      <Text style={styles.subtitle}>메뉴 2 페이지입니다.</Text>
+      <Calendar current={'2022-03-01'} onDayPress={(day) => {
+        console.log('day', day);
+      }}/>
+      {/*<Text style={styles.title}>메뉴 2</Text>*/}
+      {/*<Text style={styles.subtitle}>메뉴 2 페이지입니다.</Text>*/}
     </View>
   );
 }
